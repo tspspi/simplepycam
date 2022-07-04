626dbd375771eb93248f2d38f4e865aeff1ac86e
tspspi <11219971+tspspi@users.noreply.github.com>
2022-07-05 01:05:26 +0200

    Fixed path for README

--- cext/setup.py
+++ cext/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "../README.md").read_text()
 
 setup(
     name="simplepycam",
