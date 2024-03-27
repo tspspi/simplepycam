57eb3158cfa806af642c87cceff1913ee0d1584e
tspspi <11219971+tspspi@users.noreply.github.com>
2024-03-27 15:36:29 +0100

    Packing cext into full Python module

---
+++ cext/samples/storeframescallback.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import simplepycam
+
+nStoredImages = 0
+nImagesToStore = 100
+
+def cbStoreImage(camera, frame):
+	global nStoredImages
+	print("Storing image {} of {}".format(nStoredImages, nImagesToStore))
+
+	# Transform frame into flatter array so PIL is able to process it
+	newPilImage = []
+	for row in range(len(frame)):
+		for col in range(len(frame[row])):
+			newPilImage.append(frame[row][col])
+
+	im = Image.new(mode="RGB", size=(len(frame[0]), len(frame)))
+	im.putdata(newList)
+	im.save("image{}.jpg".format(nStoredImages))
+
+	nStoredImages = nStoredImages + 1
+	if(nStoredImages >= nImagesToStore):
+		return False
+
+	return True
+
+with simplepycam.Camera("/dev/video0") as cam:
+	cam.frameCallback = [ cbStoreImage ]
+	cam.stream()
--- cext/setup.py
+++ cext/setup.py
@@ -2,16 +2,17 @@ from distutils.core import setup, Extension
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "../README.md").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name="simplepycam",
+    name="simplepycam-tspspi",
     version="0.1",
 	author='Thomas Spielauer',
+    author_email="pypipackages01@tspi.at",
 	description='Simple Python native access library for Video4Linux camera devices',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
-	license_files='../LICENSE.md',
+	license_files='LICENSE.md',
 	url='https://github.com/tspspi/simplepycam',
 
 	python_requires='>=3.6',
@@ -31,6 +32,14 @@ setup(
 
 		'Development Status :: 3 - Alpha'
 	],
+
+    setup_requires=[
+        "setuptools-git-versioning<2"
+    ],
+
+    setuptools_git_versioning={
+        "enabled" : True
+    },
 
     ext_modules=[
         Extension("simplepycam", ["simplepycam.c"], include_dirs=["/usr/local/include"])
